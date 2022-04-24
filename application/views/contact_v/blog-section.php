b7ee7d213f0f828584a105701e027965f7274dce
MCTMNKS <75638101+MCTMNKS@users.noreply.github.com>
2022-04-25 01:48:00 +0300

    Kitaplar.php Parçalandı

---
+++ application/views/contact_v/blog-section.php
@@ -0,0 +1,49 @@
+<section class="blog">
+    <div class="container">
+      <div class="row">
+        <div class="title text-center">
+          <h2>Our Blog</h2>
+        </div>
+        <div class="col-md-9">
+          <!-- Blog Left Sections 
+          =========================-->
+
+
+          <!-- See All Post -->
+          <div class="col-md-12">
+            <div class="see-all-post text-center">
+              <a class="btn btn-default th-btn solid-btn" href="#" role="button">See All Posts <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <!-- Blog Right Sections 
+          =========================-->
+          	<div class="blog-sidbar">
+		<div class="search widgets">
+			<form class="form-inline">
+			  <div class="form-group search-input">
+			    <input type="text" class="form-control" placeholder="Search ...">
+			  </div>
+			  <button type="submit" class="btn btn-default tf-search-btn"><i class="tf-search"></i></button>
+			</form>
+		</div>
+		<div class="categories widgets">
+			<div class="list-group text-center">
+				<div class="list-group-item active"> Blog Categories </div>
+				<a href="#" class="list-group-item">Web Design</a>
+				<a href="#" class="list-group-item">User Interface</a>
+				<a href="#" class="list-group-item">User Experience</a>
+				<a href="#" class="list-group-item">Typography</a>
+				<a href="#" class="list-group-item">Color Sense</a>
+				<a href="#" class="list-group-item">Future Trend</a>
+				<a href="#" class="list-group-item">Modern Design</a>
+			</div>
+		</div>
+
+	</div>
+        </div>
+
+      </div>
+    </div>
+  </section>
