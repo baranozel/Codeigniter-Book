249a9348ec8158487769aa714bb8f036125925c2
MCTMNKS <75638101+MCTMNKS@users.noreply.github.com>
2022-04-25 01:52:04 +0300

    aaa

--- application/views/books_v.php
+++ application/views/books_v.php
@@ -2,10 +2,10 @@
 
 <?php $this->load->view("kitap_v/loader"); ?>
 
-<?php $this->load->view("contact_v/contact_hero.php"); ?>
+<?php $this->load->view("books_v/books-hero.php"); ?>
 
 <?php $this->load->view("kitap_v/navbar"); ?>
 
-<?php $this->load->view("contact_v/blog-section.php"); ?>
+<?php $this->load->view("books_v/books-section.php"); ?>
 
 <?php $this->load->view("kitap_v/footer"); ?>
